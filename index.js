17c1df15644940e2a4db77e290767042a258195a
bhaskar <bhaskarkushwah32@gmail.com>
2025-08-04 14:23:36 +0530

    Changes make index file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Welcome\n');
+  res.end('Welcome to Elevate Labs Devops Internship!\n');
 });
 
 server.listen(port, () => {
